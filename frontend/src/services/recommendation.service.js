cd4c15d51db6c2cd3e6ceb2bd1dce4827845129c
JeanLuca999 <jeanescolaetec2019@gmail.com>
2025-07-28 20:47:35 -0300

    feat: implement getRecommendations service

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,12 +1,99 @@
 // getRecommendations.js
 
 const getRecommendations = (
-  formData = { selectedPreferences: [], selectedFeatures: [] },
+  formData = {
+    selectedPreferences: [],
+    selectedFeatures: [],
+    selectedRecommendationType: '',
+  },
   products
 ) => {
-  /**
-   * Crie aqui a lógica para retornar os produtos recomendados.
-   */
+  const { selectedPreferences, selectedFeatures, selectedRecommendationType } =
+    formData;
+  const hasPreferencesAndFeaturesSelected =
+    selectedPreferences?.length && selectedFeatures?.length;
+
+  const hasOnlyPreferencesSelected =
+    selectedPreferences?.length && !selectedFeatures?.length;
+
+  const hasOnlyFeaturesSelected =
+    !selectedPreferences?.length && selectedFeatures?.length;
+
+  if (hasPreferencesAndFeaturesSelected) {
+    const filteredProducts = filterForPreferencesAndFeatures(
+      products,
+      selectedPreferences,
+      selectedFeatures
+    );
+
+    return selectedItemsBasedOnRecommendationType(
+      filteredProducts,
+      selectedRecommendationType
+    );
+  }
+
+  if (hasOnlyPreferencesSelected) {
+    const filteredProducts = filterForPreferencesOnly(
+      products,
+      selectedPreferences
+    );
+    return selectedItemsBasedOnRecommendationType(
+      filteredProducts,
+      selectedRecommendationType
+    );
+  }
+
+  if (hasOnlyFeaturesSelected) {
+    const filteredProducts = filterForFeaturesOnly(products, selectedFeatures);
+    return selectedItemsBasedOnRecommendationType(
+      filteredProducts,
+      selectedRecommendationType
+    );
+  }
 };
 
 export default { getRecommendations };
+
+function filterForPreferencesAndFeatures(
+  products,
+  selectedPreferences,
+  selectedFeatures
+) {
+  const filteredProducts = products.filter((product) => {
+    const { preferences, features } = product;
+    return (
+      preferences.some((preference) =>
+        selectedPreferences.includes(preference)
+      ) && features.some((feature) => selectedFeatures.includes(feature))
+    );
+  });
+  return filteredProducts;
+}
+
+function filterForPreferencesOnly(products, selectedPreferences) {
+  const filteredProducts = products.filter((product) => {
+    const { preferences } = product;
+    return preferences.some((preference) =>
+      selectedPreferences.includes(preference)
+    );
+  });
+  return filteredProducts;
+}
+
+function filterForFeaturesOnly(products, selectedFeatures) {
+  const filteredProducts = products.filter((product) => {
+    const { features } = product;
+    return features.some((feature) => selectedFeatures.includes(feature));
+  });
+  return filteredProducts;
+}
+
+function selectedItemsBasedOnRecommendationType(products, recommendation) {
+  if (recommendation === 'SingleProduct' && products.length > 1) {
+    return [products[products.length - 1]];
+  }
+  if (recommendation === 'SingleProduct') {
+    return [products[0]];
+  }
+  return products;
+}
